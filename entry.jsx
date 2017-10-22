f05bc1a8390a61aa35946509249e2f9269e809be
Alex Kornfeld <akorn3000@gmail.com>
2017-10-21 21:30:35 -0400

    Set up webpack, package json

---
+++ entry.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CodingTest from './coding_test';
+
+document.addEventListener("DomContentLoaded", () => {
+  const root = document.getElementById('root');
+  ReactDom.render(<CodingTest />, root);
+});
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+// webpack.config.js
+var path = require('path');
+
+module.exports = {
+  context: __dirname,
+  entry: './entry.jsx',
+  output: {
+    path: "./",
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-maps',
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  }
+};
